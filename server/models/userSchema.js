854b9dc4ef0d1796dc4eaf532b986f893cd84fec
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-24 01:13:12 +0600

    in registration pass hashing complete

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,27 +1,45 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  fullName: { 
-    type: String, 
-    required: true 
+
+  fullName: {
+    type: String,
+    required: true
   },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true, 
-    lowercase: true 
-  }, 
-   password: { 
-    type: String, 
-    required: true 
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
   },
- 
-}, { 
+
+  password: {
+    type: String,
+    required: true
+  }
+
+}, {
   timestamps: true
 });
 
 
 
-module.exports= mongoose.model('User', userSchema);
+userSchema.pre('save', async function () {
 
 
+  if (!this.isModified('password')) {
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(
+    this.password,
+    salt
+  );
+});
+
+
+module.exports = mongoose.model('User', userSchema);
