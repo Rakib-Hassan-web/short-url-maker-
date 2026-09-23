764e43b0e3655a3a97b1199708208423999d2159
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-24 01:38:14 +0600

    shortner schema creating st

---
+++ server/models/shortnerSchema.js
@@ -0,0 +1,18 @@
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose');
+
+
+const shortnerSchema = new mongoose.Schema({
+    longUrl:{
+        type:String,
+        required:true
+    },
+    shortUrl:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
