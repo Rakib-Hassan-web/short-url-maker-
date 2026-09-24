852466a08591169c054944aea914b250e49afa3d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-24 18:16:56 +0600

    short url maker redirect page working properly but need to fix all of other problems

--- server/controllers/shortnerController.js
+++ server/controllers/shortnerController.js
@@ -29,7 +29,19 @@ const shortUrlcreate = async(req,res)=>{
 
 const redirectToUrl = async (req,res)=>{
 
+    const params = req.params
+
+    if(!params.id) return sendError(res , "No  Valid url" ,400)
+
+
+    const urlData = await shortnerSchema.findOne({shortUrl : params.id})
+
+
+    // sendSuccess.redirect(res,  urlData ,200)
+
+    res.redirect(urlData.longUrl)
+    
 }
 
 
-module.exports={shortUrlcreate}
+module.exports={shortUrlcreate,redirectToUrl}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routee =express.Router()
 const auth_route = require("./authRoutes")
-const shortner_route = require("./shortner")
+const shortner_route = require("./shortner");
+const { redirectToUrl } = require('../controllers/shortnerController');
 
 
 routee.use("/auth" ,auth_route)
 routee.use("/url" ,shortner_route)
 
-routee.get("/:id" ,)
+routee.get("/:id" ,redirectToUrl)
 
 
 module.exports=routee
