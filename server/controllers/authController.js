76973b8ade66e18b6ee363a4d5c15e9d0c3db437
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2026-09-25 21:52:31 +0600

    jwt token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -69,8 +69,8 @@ const login  = async(req,res)=>{
 
            if(!isMatch)  return sendError(res , "password not match" ,400) 
 
-            jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
-  console.log(token);
+            jwt.sign({  }, {
+ 
 });
 
 
